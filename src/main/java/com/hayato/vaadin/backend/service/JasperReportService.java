4474aa4a993e1cfeb0385eaca800a70b4eca3dd4
DESKTOP-91B5BTU\Admin <r.hayato@gmail.com>
2024-10-21 01:21:43 +0700

    update

---
+++ src/main/java/com/hayato/vaadin/backend/service/JasperReportService.java
@@ -0,0 +1,60 @@
+package com.hayato.vaadin.backend.service;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class JasperReportService {
+    @Value("${dir.path-output}")
+    private String pathOutput;
+
+    public JasperReportService() {
+    }
+
+    public void createPdfFile(String url, Map<String, Object> params, Connection conn, String fileName)
+            throws JRException, SQLException {
+        try{
+//            System.out.println("start generating "+ fileName);
+            long startTime = System.nanoTime();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(url, params, conn);
+            SimplePdfExporterConfiguration config = new SimplePdfExporterConfiguration();
+            JRPdfExporter exporter = new JRPdfExporter();
+            if ( (boolean) params.get("isClientStatement")){
+                String notsusUrl = new ClassPathResource("templates/jasper/ClientStatement/"+ "NotasiKhusus" + ".jasper").getURL().getPath();
+//                String notsusUrl = new ClassPathResource("templates/jasper/ClientStatement/"+ "NotasiKhusus" + ".jasper").getURL().getPath();
+//                JasperPrint notsusPrint = JasperFillManager.fillReport(notsusUrl, params, conn);
+                List jprintlist = new ArrayList();
+                jprintlist.add(jasperPrint);
+//                jprintlist.add(notsusPrint);
+                exporter.setExporterInput(SimpleExporterInput.getInstance(jprintlist));
+            }
+
+            String filePath = pathOutput + fileName + ".pdf";
+            exporter.setConfiguration(config);
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(filePath));
+
+            exporter.exportReport();
+            long endTime   = System.nanoTime();
+            double totalTime = (endTime - startTime)/1000000000;
+            System.out.println("done generating "+ fileName + " in " + totalTime + " seconds");
+
+        } catch (Exception e){
+            System.out.println("error generating sid " + params.get("sid"));
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/hayato/vaadin/ui/UserView.java
+++ src/main/java/com/hayato/vaadin/ui/UserView.java
@@ -2,10 +2,20 @@ package com.hayato.vaadin.ui;
 
 import com.hayato.vaadin.backend.model.entity.User;
 import com.hayato.vaadin.backend.service.NewUserService;
+import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -13,9 +23,14 @@ import com.vaadin.flow.data.renderer.NativeButtonRenderer;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.vaadin.crudui.crud.impl.GridCrud;
 
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Route("")
 public class UserView extends VerticalLayout {
@@ -71,12 +86,55 @@ public class UserView extends VerticalLayout {
     }
 
     private void deleteUser(User user) {
-        userService.deleteById(user.getUserid());
+        ConfirmDialog dialog = new ConfirmDialog();
+        dialog.setHeader("Delete User");
+        dialog.setText(
+                String.format("Are you sure you want to delete user %s?", user.getUsername()));
+
+        dialog.setCancelable(true);
+        dialog.addCancelListener(event -> dialog.close());
+
+        dialog.setConfirmText("Delete");
+        dialog.setConfirmButtonTheme("error primary");
+        dialog.addConfirmListener(event -> {
+            userService.deleteById(user.getUserid());
+            refreshGrid();
+        });
+        dialog.open();
     }
 
-    private void generateReport() {
-
-    }
+    @Async
+    private void generateReport()  {
+//        try{
+////            System.out.println("start generating "+ fileName);
+//            long startTime = System.nanoTime();
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(url, params, conn);
+//            SimplePdfExporterConfiguration config = new SimplePdfExporterConfiguration();
+//            JRPdfExporter exporter = new JRPdfExporter();
+//            if ( (boolean) params.get("isClientStatement")){
+//                String notsusUrl = new ClassPathResource("templates/jasper/ClientStatement/"+ "NotasiKhusus" + ".jasper").getURL().getPath();
+////                String notsusUrl = new ClassPathResource("templates/jasper/ClientStatement/"+ "NotasiKhusus" + ".jasper").getURL().getPath();
+////                JasperPrint notsusPrint = JasperFillManager.fillReport(notsusUrl, params, conn);
+//                List jprintlist = new ArrayList();
+//                jprintlist.add(jasperPrint);
+////                jprintlist.add(notsusPrint);
+//                exporter.setExporterInput(SimpleExporterInput.getInstance(jprintlist));
+//            }
+//
+//            String filePath = pathOutput + fileName + ".pdf";
+//            exporter.setConfiguration(config);
+//            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(filePath));
+//
+//            exporter.exportReport();
+//            long endTime   = System.nanoTime();
+//            double totalTime = (endTime - startTime)/1000000000;
+//            System.out.println("done generating "+ fileName + " in " + totalTime + " seconds");
+//
+//        } catch (Exception e){
+//            System.out.println("error generating sid " + params.get("sid"));
+//            System.out.println(e.getMessage());
+//        }
+        }
 
     private void refreshGrid() {
         grid.setItems(userService.findAll());
@@ -93,16 +151,21 @@ public class UserView extends VerticalLayout {
         }
 
         Button saveButton = new Button("Save", e -> {
+
+            if (user == null) {
+                var isExist = userService.findByUsernameOrEmail(nameField.getValue(), emailField.getValue());
+
+                if (isExist) {
+                    showNotificationError("Username or email already exists");
+                    return;
+                }
+            }
+
             var newUser = checkUser(user);
             newUser.setUsername(nameField.getValue());
             newUser.setEmail(emailField.getValue());
-
-            if (user == null) {
-                newUser.setCreateUser("System");
-                newUser.setUpdateUser("System");
-            } else {
-                newUser.setUpdateUser("System");
-            }
+            newUser.setCreateUser("System");
+            newUser.setUpdateUser("System");
 
             userService.save(newUser);
             refreshGrid();
@@ -122,5 +185,25 @@ public class UserView extends VerticalLayout {
         }
 
         return user;
+    }
+
+    private void showNotificationError(String textLabel) {
+        Notification notification = new Notification();
+        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+
+        Div text = new Div(new Text(textLabel));
+
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.setAriaLabel("Close");
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
+
+        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+        layout.setAlignItems(Alignment.CENTER);
+
+        notification.add(layout);
+        notification.open();
     }
 }
